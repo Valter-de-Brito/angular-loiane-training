4d8c0ba3f4cf2edadda8e5f389d39cae3d7dc318
Valter-de-Brito <valter.dbrito@gmail.com>
2020-09-02 18:32:00 +0100

    Create a new component & module - Two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 import { CursosModule } from './cursos/cursos.module';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
     MeuPrimeiro2Component,
     DataBindingComponent,
   ],
-  imports: [BrowserModule, CursosModule],
+  imports: [BrowserModule, CursosModule, MeuFormModule],
   providers: [],
   bootstrap: [AppComponent],
 })
