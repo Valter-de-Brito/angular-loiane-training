4bd29fce4a82414344cd600a00178826ba7ddcb8
Valter-de-Brito <valter.dbrito@gmail.com>
2020-10-06 13:39:00 +0100

    Create a new router

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { MaterializeModule } from 'angular2-materialize';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [AppComponent, CursosComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, MaterializeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
